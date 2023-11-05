cfcd3f7c2217758287af49b062ec841f64106d24
Masoud Rafiei-Ravandi <mrafieir@users.noreply.github.com>
2023-11-05 11:26:27 -0800

    backward compatible: query extra flags for sdss survey

--- frb/surveys/sdss.py
+++ frb/surveys/sdss.py
@@ -94,7 +94,7 @@ class SDSS_Survey(surveycoord.SurveyCoord):
         query = "SELECT GN.distance, "
         query += "p.objid, "
 
-        query += "pz.z as redshift, pz.zErr as redshift_error, p.expRad_g as expRad_g, p.expRad_r as expRad_r, p.deVRad_g as deVRad_g, p.deVRad_r as deVRad_r\n"
+        query += "pz.z as redshift, pz.zErr as redshift_error, p.expRad_g as expRad_g, p.expRad_r as expRad_r, p.deVRad_g as deVRad_g, p.deVRad_r as deVRad_r, p.mode as mode, p.resolveStatus as resolveStatus\n"
         query += "FROM PhotoObj as p\n"
         query += "JOIN dbo.fGetNearbyObjEq({:f},{:f},{:f}) AS GN\nON GN.objID=p.objID\n".format(
             self.coord.ra.value,self.coord.dec.value,self.radius.to('arcmin').value)
@@ -120,6 +120,8 @@ class SDSS_Survey(surveycoord.SurveyCoord):
         photom_catalog['expRad_r'] = -9999.
         photom_catalog['deVRad_g'] = -9999.
         photom_catalog['deVRad_r'] = -9999.
+        photom_catalog['mode'] = -9999.
+        photom_catalog['resolveStatus'] = -9999.
         # Fill
         if np.any(gdz):
             photom_catalog['photo_z'][matches[gdz]] = photz_cat['redshift'][np.where(gdz)]
@@ -128,6 +130,8 @@ class SDSS_Survey(surveycoord.SurveyCoord):
             photom_catalog['expRad_r'][matches[gdz]] = photz_cat['expRad_r'][np.where(gdz)]
             photom_catalog['deVRad_g'][matches[gdz]] = photz_cat['deVRad_g'][np.where(gdz)]
             photom_catalog['deVRad_r'][matches[gdz]] = photz_cat['deVRad_r'][np.where(gdz)]
+            photom_catalog['mode'][matches[gdz]] = photz_cat['mode'][np.where(gdz)]
+            photom_catalog['resolveStatus'][matches[gdz]] = photz_cat['resolveStatus'][np.where(gdz)]
 
         # Trim down catalog
         trim_catalog = trim_down_catalog(photom_catalog, keep_photoz=True)
